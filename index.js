fd1a7c81a1c163034eb2677c7296dee8a09d19ef
tanveer.chege@student.moringaschool.com <tanveer.chege@student.moringaschool.com>
2022-08-22 21:15:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+//function findMatching
+function findMatching(drivers, argument) {
+  return drivers.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === argument.toLowerCase()
+  );
+}
+//function fuzzyMatch
+function fuzzyMatch(drivers, testString) {
+  return drivers.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+//function matchName
+function matchName(drivers, argumentName) {
+  return drivers.filter((record) => record.name === argumentName);
+}
